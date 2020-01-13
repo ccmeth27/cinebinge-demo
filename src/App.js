86532551700be2550985680c04acd463fed5da4a
Connor Meth <connormeth27@gmail.com>
2020-01-13 13:20:06 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './cinebinge-logo.png';
 import './App.css';
+import SubscriptionSelect from './SubscriptionSelect'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="logo" alt="logo" />
+        <h1>Cinebinge Demo</h1>
       </header>
+      <div className="demo-container">
+        <SubscriptionSelect/>
+
+      </div>
     </div>
   );
 }
---
+++ src/SubscriptionSelect.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function SubscriptionSelect () {
+    return (
+        <div className="feature-container">
+            <div>
+                <h2>Subscription Selection Screen</h2>
+            </div>
+            <div>
+                <img/>
+            </div>
+
+        </div>
+    )
+}
